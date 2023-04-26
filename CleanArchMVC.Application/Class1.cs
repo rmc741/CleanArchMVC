4639ab114adb20d9ce549f10f5bddc398cd0c3b0
RafaelMendonca <rafael.medonca2@gmail.com>
2023-04-26 14:07:44 -0300

    Primeiro commit, criando estrutura do projeto

---
+++ CleanArchMVC.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchMVC.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ CleanArchMVC.Infra.IoC/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace CleanArchMVC.Infra.IoC
+{
+    public class Class1
+    {
+    }
+}
