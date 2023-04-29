1dae7318c22529bd1774f806d6b7d3d742aeaa76
RafaelMendonca <rafael.medonca2@gmail.com>
2023-04-29 19:19:02 -0300

    Adicinado interface para model de cateogria e produto

---
+++ CleanArchMVC.Domain/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMVC.Domain.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMVC.Domain.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetCategoriesAsync();
+        Task<Product> GetByIdAsync(int? id);
+        Task<Product> GetProductCategoryAsync(int? id);
+        Task<Product> CreateAsync(Product product);
+        Task<Product> UpdateAsync(Product product);
+        Task<Product> RemoveAsync(Product product);
+    }
+}
