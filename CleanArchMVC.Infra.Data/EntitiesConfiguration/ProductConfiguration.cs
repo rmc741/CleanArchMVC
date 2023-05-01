42266de608566218d1069d2bbeb306cbe0ab7c1b
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-01 17:28:27 -0300

    Inicializado camada de infra estrutura e adicionando classe de context e configuração

---
+++ CleanArchMVC.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,21 @@
+﻿
+using CleanArchMVC.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace CleanArchMVC.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(t => t.Id);
+            builder.Property(p => p.Name).HasMaxLength(100).IsRequired();
+            builder.Property(p => p.Description).HasMaxLength(250).IsRequired();
+            builder.Property(p => p.Price).HasPrecision(10, 2);
+
+            builder.HasOne(e => e.Category).WithMany(e => e.Products)
+                .HasForeignKey(e => e.CategoryId);
+        }
+    }
+}
