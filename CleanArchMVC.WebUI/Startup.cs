3b3ad91f1f81b7f1b8176d74e50db55b0ea8538d
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-07 12:30:55 -0300

    Criado controllers das entidades e views para testes

--- CleanArchMVC.WebUI/Startup.cs
+++ CleanArchMVC.WebUI/Startup.cs
@@ -48,7 +48,7 @@ namespace CleanArchMVC.WebUI
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Products}/{action=Index}/{id?}");
             });
         }
     }
