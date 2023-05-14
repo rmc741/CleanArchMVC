439537650d4e57825828bed3af998c52b0ca8021
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-13 22:29:40 -0300

    -

--- CleanArchMVC.WebUI/Controllers/HomeController.cs
+++ CleanArchMVC.WebUI/Controllers/HomeController.cs
@@ -8,5 +8,10 @@ namespace CleanArchMVC.WebUI.Controllers
         {
             return View();
         }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
     }
 }
