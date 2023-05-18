6bd351e398395c450bb3c184e09a873e7e9a7ff6
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-18 13:52:13 -0300

    Adicionado a camada InfraData o Identity User

--- CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
+++ CleanArchMVC.Infra.Data/Context/ApplicationDbContext.cs
@@ -1,11 +1,11 @@
-﻿
-
-using CleanArchMVC.Domain.Entities;
+﻿using CleanArchMVC.Domain.Entities;
+using CleanArchMVC.Infra.Data.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace CleanArchMVC.Infra.Data.Context
 {
-    public class ApplicationDbContext : DbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
