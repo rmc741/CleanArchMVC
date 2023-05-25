1a6cd580dcc9372e0afef356844527220a5e6f00
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-25 15:49:58 -0300

    Criado ViewModel para Login e Authetication e inicializado estrutura do controller.

---
+++ CleanArchMVC.WebUI/Controllers/AccountController.cs
@@ -0,0 +1,14 @@
+﻿using CleanArchMVC.Domain.Account;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CleanArchMVC.WebUI.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly IAuthenticate _authentication;
+        public AccountController(IAuthenticate authenticate)
+        {
+            _authentication = authenticate;
+        }
+    }
+}
