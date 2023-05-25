5a8ee8d7fc3812ca3488a8948045e9a704c55307
RafaelMendonca <rafael.medonca2@gmail.com>
2023-05-25 20:46:18 -0300

    Implementados controladores para login e registro.

--- CleanArchMVC.Domain/Account/IAuthenticate.cs
+++ CleanArchMVC.Domain/Account/IAuthenticate.cs
@@ -4,7 +4,7 @@ namespace CleanArchMVC.Domain.Account
 {
     public interface IAuthenticate
     {
-        Task<bool> Autheticate(string email, string password);
+        Task<bool> Authenticate(string email, string password);
         Task<bool> RegisterUser(string email, string password);
         Task Logout();
     }
